3164298e3464ac18edbb635227152c24422c4ad4
Jonathan Stout <jonstout@indiana.edu>
2015-02-28 16:33:12 -0500

    moving textbox to util

--- src/levels/level1.js
+++ src/levels/level1.js
@@ -7,35 +7,18 @@ Q.scene("level1",function(stage) {
     dataAsset: 'level.json',
     sheet:     'tiles' }));
 
-    // Create the player and add him to the stage
-    var player = stage.insert(new Q.Player());
+  // Create the player and add him to the stage
+  var player = stage.insert(new Q.Player());
 
-    // Give the stage a moveable viewport and tell it
-    // to follow the player.
-    stage.add("viewport").follow(player);
+  // Give the stage a moveable viewport and tell it
+  // to follow the player.
+  stage.add("viewport").follow(player);
 
-    // Load enemies from file
-    $.getJSON( "data/enemies.json", function( data ) {
-      stage.loadAssets(data["enemies"]);
-    });
-
-    // Finally add in the tower goal
-    stage.insert(new Q.Tower({ x: 180, y: 50 }));
+  // Load enemies from file
+  $.getJSON( "data/enemies.json", function( data ) {
+    stage.loadAssets(data["enemies"]);
   });
 
-  // Display a text box over game content.
-  Q.scene("textbox", function(stage) {
-
-    var container = stage.insert(new Q.UI.Container({
-      x: Q.width/2, y: Q.height/4, fill: "rgba(50,150,200,1.0)", h: 200, w: 500
-    }));
-
-    var button = container.insert(new Q.UI.Button({ x: 180, y: 60, fill: "#CCCCCC", label: "unpause" }))
-    container.insert(new Q.UI.Text({x: -120, y: -80, fill: "#CCCCCC", size: 14, label: stage.options.label}))
-
-    // TODO - Add key press event to kill textbox.
-    button.on("click", function() {
-      Q.clearStage(1)
-      Q.stage().unpause()
-    });
-  });
+  // Finally add in the tower goal
+  stage.insert(new Q.Tower({ x: 180, y: 50 }));
+});
--- src/util.js
+++ src/util.js
@@ -5,3 +5,20 @@ Q.Sprite.extend("Tower", {
     this._super(p, { sheet: 'tower' });
   }
 });
+
+// Display a text box over game content.
+Q.scene("textbox", function(stage) {
+
+  var container = stage.insert(new Q.UI.Container({
+    x: Q.width/2, y: Q.height/4, fill: "rgba(50,150,200,1.0)", h: 200, w: 500
+  }));
+
+  var button = container.insert(new Q.UI.Button({ x: 180, y: 60, fill: "#CCCCCC", label: "unpause" }))
+  container.insert(new Q.UI.Text({x: -120, y: -80, fill: "#CCCCCC", size: 14, label: stage.options.label}))
+
+  // TODO - Add key press event to kill textbox.
+  button.on("click", function() {
+    Q.clearStage(1)
+    Q.stage().unpause()
+  });
+});
