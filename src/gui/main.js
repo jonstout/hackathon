170cc5508e73189e640d7ca4f93bf4cbb5f71ef6
Jonathan Stout <jonstout@indiana.edu>
2015-02-28 18:45:48 -0500

    adding main menu

---
+++ src/gui/main.js
@@ -0,0 +1,20 @@
+
+// Create the main menu scene
+Q.scene("mainMenu", function(stage) {
+  var container = stage.insert(new Q.UI.Container({
+    x: Q.width/2, y: Q.height/2, fill: "rgba(150,25,25,1.0)", h: 200, w: 500
+  }));
+
+  var playButton = container.insert(new Q.UI.Button({ x: 0, y: -30, fill: "#CCCCCC", label: "Play" }))
+  var createButton = container.insert(new Q.UI.Button({ x: 0, y: 30, fill: "#CCCCCC", label: "Create" }))
+
+  playButton.on("click", function() {
+    Q.clearStage(window.mainMenuScene)
+    Q.stageScene("level1");
+  });
+
+  createButton.on("click", function() {
+    Q.clearStage(window.mainMenuScene)
+    Q.stageScene("level1");
+  });
+});
