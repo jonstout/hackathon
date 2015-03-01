f009858dbb4f1558d767e7b2aec458d50d3b199c
Carter <carterw8989@hotmail.com>
2015-02-28 23:40:58 -0500

    compiled enemy type

--- src/enemy.js
+++ src/enemy.js
@@ -54,3 +54,35 @@ Q.Sprite.extend("Enemy",{
     }
 }
 });
+
+Q.Sprite.extend("verticalEnemy", {
+    init: function (p) {
+        this._super(p, { vy: -100, rangeY: 50, gravity: 0 });
+        this.add("2d");
+
+        this.p.initialY = this.p.y;
+
+        this.on("bump.left,bump.right,bump.bottom,bump.top", function (collision) {
+            if (collision.obj.isA("Player")) {
+                Q.stageScene("endGame", 1, { label: "Game Over" });
+                collision.obj.destroy();
+            }
+        });
+
+        this.on("bump.top", function (collision) {
+            if (collision.obj.isA("Player")) {
+                collision.obj.p.vy = -100;
+                this.destroy();
+            }
+        });
+    },
+    step: function (dt) {
+        if (this.p.y - this.p.initialY >= this.p.rangeY && this.p.vy > 0) {
+            this.p.vy = -this.p.vy;
+        }
+        else if (-this.p.y + this.p.initialY >= this.p.rangeY && this.p.vy < 0) {
+            this.p.vy = -this.p.vy;
+        }
+    }
+
+});
