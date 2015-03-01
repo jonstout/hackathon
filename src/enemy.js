c38d74a3f5b95eca47637ce9328ea955a4f2dc75
Jonathan Stout <jonstout@indiana.edu>
2015-03-01 00:08:47 -0500

    adding enemy logic

--- src/enemy.js
+++ src/enemy.js
@@ -26,6 +26,33 @@ Q.Sprite.extend("Enemy",{
         collision.obj.p.vy = -300;
       }
     });
+  },
+
+  step: function(dt) {
+    var dirX = this.p.vx/Math.abs(this.p.vx);
+    var ground = Q.stage().locate(this.p.x, this.p.y + this.p.h/2 + 1, Q.SPRITE_DEFAULT);
+    var nextTile = Q.stage().locate(this.p.x + dirX * this.p.w/2 + dirX, this.p.y + this.p.h/2 + 1, Q.SPRITE_DEFAULT);
+
+    //if we are on ground and there is a cliff
+    if(!nextTile && ground) {
+      if(this.p.vx > 0) {
+        if(this.p.defaultDirection == "right") {
+          this.p.flip = "x";
+        }
+        else {
+          this.p.flip = false;
+        }
+      }
+      else {
+        if(this.p.defaultDirection == "left") {
+          this.p.flip = "x";
+        }
+        else {
+          this.p.flip = false;
+        }
+      }
+      this.p.vx = -this.p.vx;
+    }
   }
 });
 
